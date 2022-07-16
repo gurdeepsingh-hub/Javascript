a21c7dfb125e0f31a43c0323823d0965eddd9327
guri <khalsagudeep@gmail.com>
2022-07-16 18:25:27 +0530

    Some basic tasks Completed

--- reverse_string_words.js
+++ reverse_string_words.js
@@ -4,10 +4,10 @@ function wordReverse (str) {
     let reverseString ='';
     for(let i=0;i<str.length;i++){
 
-        if(str.charAt(i) == (/[a-z]/i) )
+        if(str.charAt(i) == /a-z/i )
         {
             stack.push(str.charAt(i));
-        }else if(str.charAt(i) != (/[a-z]/i) ){
+        }else if(str.charAt(i) != /a-z/i ){
             while(stack.length > 0){
                 reverseString += stack.pop();
             }
